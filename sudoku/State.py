64efac332486a3476df1751ab434f24f345832f0
vonHousen <42911623+vonHousen@users.noreply.github.com>
2020-01-21 23:20:24 +0100

    Added some TODOs.

--- sudoku/State.py
+++ sudoku/State.py
@@ -14,6 +14,9 @@ class State:
 		:param sudoku:	Sudoku object which is connected with current state.
 		"""
 		self._sudoku = Sudoku(sudoku.board, sudoku.hardcoded_digits)
+		self._best_conflict_count = self.sudoku.size ** 2
+		self._conflict_count = self.sudoku.size ** 2
+		self._update_conflict_count()
 
 	@property
 	def sudoku(self):
@@ -31,6 +34,20 @@ class State:
 		while old_digit == digit:
 			digit = np.random.randint(low=1, high=self.sudoku.size, size=1)
 		self.sudoku.update_digit(position_to_edit=position, new_digit=digit)
+
+	def _update_conflict_count(self):
+		"""
+		Counts conflict's count - f(state) function in documentation.
+		Updates both _conflict_count & _best_conflict_count
+		"""
+		# TODO should update both _best_conflict_count & _conflict_count
+
+	def is_this_promising_state(self):
+		"""
+		Returns True if just updated _conflict_count is better (lower) than calculated _best_conflict_count so far.
+		:return: boolean if it is a promising state.
+		"""
+		# TODO among others: should call _update_conflict_count()
 
 
 if __name__ == "__main__":
